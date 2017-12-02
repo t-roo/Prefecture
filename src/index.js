1122a8bd1c836c0cee5e8b49fcf93abcb4cd574b
roo <wrzxasqw@yahoo.co.jp>
2017-12-02 13:58:01 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
 import CrimeDisplay from './CrimeDisplay';
 
 ReactDOM.render(<CrimeDisplay />, document.getElementById('root'));
